9fcdd839318ee42f9cd5f5d97741634e15e406c7
Ahmed Esaa <ahmedelsaiyed2@gmail.com>
2025-02-10 17:43:51 +0200

    Complete the home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./Components/Header/header.component";
+import { FooterComponent } from "./Components/Footer/footer.component";
+import { MenuComponent } from "./Components/menu/menu.component";
+import { OfferComponent } from "./Components/offer/offer.component";
+import { AboutComponent } from "./Components/about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MenuComponent, OfferComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
